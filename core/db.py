6162fe1446c2934b6ffef6707573ac99a4ea1a2e
telemarkskihire-source <telemarkskihire@gmail.com>
2025-12-01 00:38:00 +0100

    Update db.py

--- core/db.py
+++ core/db.py
@@ -1,1 +1,21 @@
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+# Per sviluppo usiamo un semplice SQLite locale
+DATABASE_URL = "sqlite:///sci_club_val_dayas.db"
+
+engine = create_engine(
+    DATABASE_URL,
+    echo=False,
+    future=True,
+)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False,
+    future=True,
+)
+
+Base = declarative_base()
