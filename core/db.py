9c6e89d5f1bd5dc04b5444a8e2994c889041c2bf
telemarkskihire-source <telemarkskihire@gmail.com>
2025-12-01 01:52:24 +0100

    Update db.py

--- core/db.py
+++ core/db.py
@@ -1,21 +1,12 @@
-
+# core/db.py
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Per sviluppo usiamo un semplice SQLite locale
-DATABASE_URL = "sqlite:///sci_club_val_dayas.db"
+# Nuovo file DB per la versione con richieste ski-room / auto
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sci_club_v2.db"
 
 engine = create_engine(
-    DATABASE_URL,
-    echo=False,
-    future=True,
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
-
-SessionLocal = sessionmaker(
-    bind=engine,
-    autoflush=False,
-    autocommit=False,
-    future=True,
-)
-
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
